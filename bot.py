1986031f809f896757c4181a33e82c94adff2b45
reda-bouzad <115048247+reda-bouzad@users.noreply.github.com>
2023-02-28 00:51:19 +0100

    Add files via upload

--- bot.py
+++ bot.py
@@ -8,16 +8,19 @@ from chatterbot.trainers import ListTrainer
 # logging.basicConfig(level=logging.INFO)
 
 # Create a new instance of a ChatBot
+# Create a new instance of a ChatBot
 bot = ChatBot(
     'Terminal',
     storage_adapter='chatterbot.storage.SQLStorageAdapter',
     logic_adapters=[
-        'chatterbot.logic.BestMatch'
+        {
+            'import_path': 'chatterbot.logic.BestMatch',
+            'default_response': 'I am sorry, but I do not understand.',
+            'maximum_similarity_threshold': 0.6
+        }
     ],
     database_uri='sqlite:///database.db'
-    
 )
-
 # Define a list of sample dialogues to train the bot
 training_data = [
     "Hello",
@@ -31,6 +34,38 @@ training_data = [
     "My name is Bot.",
     "Who created you?",
     "I was created by Reda , Tarik , Abdelillah.",
+    "Who created you",
+    "I was created by Reda , Tarik , Abdelillah.",
+    "Qui t'a créé",
+    "par Reda , Tarik , Abdelillah",
+    "Qui a créé ce chatBot ?",
+    "j'étais créé par Reda bouzad et Abdelillah boulgha et Tariq elqari .",
+    "Quelle est la date de création de ce chatBot ?",
+    "Ce chatbot a été créer en février 2023.",
+    "Quel est le but de créer ce chatBot?",
+    "Ce chatbot recommande des documentaires.",
+    "Dans quelle filière étudient Reda, Abdelilah et Tariq ?",
+    "ils étudient dans la filiére SIR",
+    "Qu'est-ce que SIR",
+    "SIR est l'abréviation de SYSTEMES INFORMATIQUES REPARTIS",
+    "Dans quelle faculté se trouve cette branche ?",
+    "C'est qui le doyen de cette faculté ?"
+    "Le doyen de cette faculté est M. Mohamed JBILOU."
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 ]
 
@@ -57,3 +92,5 @@ while True:
     # Press ctrl-c or ctrl-d on the keyboard to exit
     except (KeyboardInterrupt, EOFError, SystemExit):
         break
+
+bot.storage.drop()
